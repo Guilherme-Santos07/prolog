052be3a694e9eba258bbda97e74fb5b9cc22f5bd
Guilherme Santos <gui071003@gmail.com>
2022-07-20 16:45:10 -0300

    tentativa de fazer o 'subconjunto' do roteiro 6

--- roteiros/roteiro-06/exc-005.pl
+++ roteiros/roteiro-06/exc-005.pl
@@ -1,10 +1,16 @@
 subconjunto([],_).
 
+/*
 subconjunto([H|T],Lista):-
     member(H,Lista),
     subconjunto(T,Lista).
+*/
 
+subconjunto([H|_],[H]).
+subconjunto([_|T],Lista):-
+    subconjunto(T,Lista).
 
+/*
 insere(Elem,Lista,[Elem|Lista]).
 insere(Elem,[H|T],[H|Lista]):-
     insere(Elem,T,Lista).
@@ -15,3 +21,4 @@ pertence(Elem,[Elem|T],[Elem|Cauda]):-
 pertence(Elem,[H|T],Lista):-
     H\=Elem,
     pertence(Elem,T,Lista).
+*/
